b6267800d93087becb3750463972204921348675
Cyrillo46 <yahooisbetter46@gmail.com>
2023-07-22 13:16:58 -0400

    images retrieved

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,21 +1,26 @@
-import memesData from "../MemesData";
+import React from "react";
+import memesData from "../MemesData.jsx";
 
 export default function Meme() {
-  function getMeme() {
-    const meme = memesData.data.memes;
-    const randomNumber = Math.random(Math.floor() * meme.length);
-    console.log(meme[randomNumber].url);
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const meme = memesArray[randomNumber].url;
+    setMemeImage(meme);
   }
 
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button" onClick={getMeme}>
-          Get a new meme image
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
         </button>
-      </form>
+      </div>
+      <img src={memeImage} />
     </main>
   );
 }
